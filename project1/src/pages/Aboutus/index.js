892aaba1f7e3a565f7e44d021c06129a846d2451
Aravind Bongoni <aravindbongoni835@gmail.com>
2023-03-29 15:54:19 +0530

    portfolio created succesfully

---
+++ project1/src/pages/Aboutus/index.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+
+export default function Aboutus(){
+    return(
+        <>
+<Typography>Name: BONGONI ARAVIND</Typography>
+<Typography>Father Name: SAMMAIAH</Typography>
+<Typography>Mother Name: RAMA</Typography>
+<Typography>Email: aravindbongoni835@gmail.com</Typography>
+<Typography>Phone No.: 9963390845</Typography>
+        </>
+        
+    )
+    }
