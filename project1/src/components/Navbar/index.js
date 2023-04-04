36301e83862a06cf90c8ecd34adff4414a354e39
Aravind Bongoni <aravindbongoni835@gmail.com>
2023-04-04 11:56:14 +0530

    portfolio completd

--- project1/src/components/Navbar/index.js
+++ project1/src/components/Navbar/index.js
@@ -14,7 +14,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { Link } from 'react-router-dom';
 
-const pages = ['Aboutus', 'Qualifications', 'Hobbies','Experience','Skills'];
+const pages = ['Aboutus', 'Qualifications', 'Hobbies','Experience','Skills',"Certifications"];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function Navbar() {
@@ -56,7 +56,7 @@ function Navbar() {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            ARAVIND
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -116,7 +116,7 @@ function Navbar() {
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
-                <Link to={page}>
+                <Link to={page}style={{textDecoration:"none"}}>
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
