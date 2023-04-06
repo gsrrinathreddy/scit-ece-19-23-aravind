94c8ed410ee36c3efaa26d58e4c3d7be46403089
Aravind Bongoni <aravindbongoni835@gmail.com>
2023-04-06 16:09:37 +0530

    Server is adding

--- project1/src/pages/Projects/index.js
+++ project1/src/pages/Projects/index.js
@@ -1,12 +1,28 @@
 import { Typography } from '@mui/material';
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+import {CircularProgress} from '@mui/material';
 
 export default function Projects(){
-        
+        let [loader,setLoader]=useState(true);
+        let [Projects,setProjects]=useState(null);
+
+        const getProjectsData = async ()=> axios.get('http://localhost:8000/Projects')
+                              .then(res=>{
+                                    setProjects(res.data)
+                                    setLoader(false)
+                              }).Catch(err => console.log(err))
+
+
+              useEffect(()=>{
+                getProjectsData();
+              },[])
+              console.log("Projects",Projects)
                               
     return(
         <>
-<Typography>1.AUTOMATIC ENGINE LOCKING SYSTEM FOR DRUNKEN DRIVERS</Typography>
-<Typography>2.EMPLOYEE LOGIN/LOGOUT SYSTEM USING RFID USING IOT  </Typography>
+<Typography>1.Automatic Locking system for drunken drivers</Typography>
+<Typography>2.Employee Login/Logout System Using RFID Using IOT</Typography>
 
         </>   
     )
